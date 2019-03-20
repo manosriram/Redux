01128e1f2c4d87cacbd48c91ebf7f49d28ef2b6d
Mano Sriram <mano.sriram0@gmail.com>
2019-03-20 17:21:15 +0530

    Added New Folders and first Commit.

--- reactRedux/src/actions/postActions.js
+++
@@ -1,9 +0,0 @@
-import { FETCH_POSTS, NEW_POST } from "../types";
-
-return function fetchPosts() {
-    return function(dispatch) {
-        const res1 = await fetch("https://jsonplaceholder.typicode.com/posts");
-        const res2 = await res1.json();
-        this.setState({ posts: res2 });
-    }
-}
